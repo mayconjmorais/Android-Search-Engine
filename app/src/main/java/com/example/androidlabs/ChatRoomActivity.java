1fa198da3717f3e55ea1a4477e6c297fbdaf995b
Maycon Morais <mayconjmorais@gmail.com>
2020-06-18 18:35:19 -0400

    Finished lab 4

--- app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
+++ app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
@@ -1,6 +1,5 @@
 package com.example.androidlabs;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,7 +7,6 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -19,61 +17,58 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import java.util.ArrayList;
 
 public class ChatRoomActivity extends AppCompatActivity {
-    private ArrayList<ChatMessage> elements = new ArrayList<>();
-    private ListView myList;
+    private ChatRoomAdapter myAdapter;
+    private ArrayList<Message> elements = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
 
-        myList = findViewById(R.id.theListView);
-        ChatRoomAdapter myAdapter = new ChatRoomAdapter(getApplicationContext(), elements);
+
+        ListView myList = findViewById(R.id.theListView);
+        EditText message = findViewById(R.id.chatMessage);
+
+        // girl button
+        Button send = findViewById(R.id.senderButton);
+        send.setOnClickListener(sd -> {
+            String messageText = message.getText().toString();
+
+            elements.add(new Message(messageText, true));
+            myAdapter.notifyDataSetChanged();
+            message.setText("");
+
+        });
+
+        // Soldier button
+        Button receive = findViewById(R.id.receiverButton);
+        receive.setOnClickListener(rc -> {
+            String messageText = message.getText().toString();
+
+            elements.add(new Message(messageText, false));
+            myAdapter.notifyDataSetChanged();
+            message.setText("");
+        });
+
+        myList.setAdapter(myAdapter = new ChatRoomAdapter(elements));
 
         // AlertDialog
         myList.setOnItemLongClickListener((parent, view, pos, id) -> {
             AlertDialog.Builder alert = new AlertDialog.Builder(this);
             String alertMsg = R.string.alertmsg1 + pos + "\n" + R.string.alertmsg2;
             alert.setTitle(R.string.alertMessage)
-                    .setMessage(R.string.alertmsg1)
+                    .setMessage("The selected row is: " + pos + "\nThe database id id: ")
                     .setNegativeButton(R.string.no, (click, arg) -> {
                     })
                     .setPositiveButton(R.string.yes, (click, arg) -> {
                         elements.remove(pos);
-                     myAdapter.notifyDataSetChanged();
+                        myAdapter.notifyDataSetChanged();
                     });
             alert.show();
             return false;
         });
 
-        // girl button
-        Button send = findViewById(R.id.senderButton);
-        send.setOnClickListener(sd -> {
-            EditText message = findViewById(R.id.chatMessage);
-
-            // send message when type something
-            if (!message.getText().toString().equals("")) {
-                myList.setAdapter(myAdapter);
-                elements.add(new ChatMessage(message.getText().toString(), true));
-                myAdapter.notifyDataSetChanged();
-                message.setText("");
-            }
-        });
-
-        // Soldier button
-        Button receive = findViewById(R.id.receiverButton);
-        receive.setOnClickListener(rc -> {
-            EditText message = findViewById(R.id.chatMessage);
-
-            // send message when type something
-            if (!message.getText().toString().equals("")) {
-                myList.setAdapter(myAdapter);
-                elements.add(new ChatMessage(message.getText().toString(), false));
-                myAdapter.notifyDataSetChanged();
-                message.setText("");
-            }
-        });
-        // TODO fudeo
+        // Refresh function
         SwipeRefreshLayout refreshLayout = findViewById(R.id.refresher);
         refreshLayout.setOnRefreshListener(() -> {
             //on refresh function()
@@ -83,24 +78,20 @@ public class ChatRoomActivity extends AppCompatActivity {
     }
 
     public class ChatRoomAdapter extends BaseAdapter {
-        private Context context;
-        private LayoutInflater inflater;
-        private ArrayList chatMessage;
+        ArrayList<Message> m;
 
-        public ChatRoomAdapter(Context context, ArrayList<ChatMessage> chatMessage) {
-            this.context = context;
-            this.chatMessage = chatMessage;
-            this.inflater = getLayoutInflater();
+        public ChatRoomAdapter(ArrayList<Message> messageList) {
+            this.m = messageList;
         }
 
         @Override
         public int getCount() {
-            return elements.size();
+            return m.size();
         }
 
         @Override
-        public Object getItem(int position) {
-            return elements.get(position);
+        public String getItem(int position) {
+            return m.get(position).getMessage();
         }
 
         @Override
@@ -111,33 +102,29 @@ public class ChatRoomActivity extends AppCompatActivity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             View newView = convertView;
-            TextView  messageText;
+            TextView text;
+            LayoutInflater inflater = getLayoutInflater();
 
-            if ( newView == null){
-                ChatMessage cm = (ChatMessage) chatMessage.get(position);
-                if (cm != null ){
-                    if (cm.isSender()) {
-                        newView = inflater.inflate(R.layout.activity_chat_sender, null);
-                        messageText = (TextView)newView.findViewById(R.id.textChat);
-                    } else {
-                        newView = inflater.inflate(R.layout.activity_chat_receiver, null);
-                        messageText = (TextView)newView.findViewById(R.id.textChatRec);
-                    }
-                    messageText.setText(cm.getMessage());
-
-                }
+            if (m.get(position).isSide()) {
+                newView = inflater.inflate(R.layout.activity_chat_sender, parent, false);
+                text = (TextView) newView.findViewById(R.id.textChat);
+                text.setText(getItem(position));
+            } else {
+                newView = inflater.inflate(R.layout.activity_chat_receiver, parent, false);
+                text = (TextView) newView.findViewById(R.id.textChatRec);
+                text.setText(getItem(position));
             }
             return newView;
         }
     }
 
-    class ChatMessage {
-        public String message;
-        public boolean sender;
+    public class Message {
+        String message;
+        boolean side;
 
-        public ChatMessage(String message, boolean sender) {
+        public Message(String message, boolean side) {
             this.message = message;
-            this.sender = sender;
+            this.side = side;
         }
 
         public String getMessage() {
@@ -148,12 +135,12 @@ public class ChatRoomActivity extends AppCompatActivity {
             this.message = message;
         }
 
-        public boolean isSender() {
-            return sender;
+        public boolean isSide() {
+            return side;
         }
 
-        public void setSender(boolean sender) {
-            this.sender = sender;
+        public void setSide(boolean side) {
+            this.side = side;
         }
     }
 
